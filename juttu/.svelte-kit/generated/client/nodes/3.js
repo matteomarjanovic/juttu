7b908f4470f736b763c81daf19c3c107d668755f
matteomarjanovic <matteomarjanovic@gmail.com>
2026-03-08 18:58:43 +0100

    Remove .svelte-kit folder from git.

--- juttu/.svelte-kit/generated/client/nodes/3.js
+++
@@ -1,3 +0,0 @@
-import * as universal from "../../../../src/routes/(app)/comments/[userHandle]/[articleId]/+page.ts";
-export { universal };
-export { default as component } from "../../../../src/routes/(app)/comments/[userHandle]/[articleId]/+page.svelte";
