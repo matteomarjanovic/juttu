348b77787e5bc480b561e258ae7e958cea5bcaec
matteomarjanovic <matteomarjanovic@gmail.com>
2026-03-17 23:04:23 +0100

    Remove index.mdx and add redirects of home to installation page.

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -25,6 +25,10 @@ export default defineConfig({
         }),
     ],
 
+    redirects: {
+        '/': '/getting-started/installation/',
+    },
+
     vite: {
         plugins: [tailwindcss()],
     },
